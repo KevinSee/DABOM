e494e5e893e6a56939ee4ae3c1893a5c1b393c82
Mike Ackerman <mackerman@qcinc.org>
2017-03-20 19:27:17 -0600

    Added LGTrappingDB data downloaded on 03/19/2017

---
+++ R/LGTrappingDB_03192017.R
@@ -0,0 +1,10 @@
+#' @title LGTrappingDB
+#'
+#' @description This is the Lower Granite Dam Trapping Database exported from the QCI download site on 03-17-19. This
+#' contains all records in the LGTrapping database including both adults and juveniles and from all spawn years.
+#' @name LGTrappingDB_03192017
+#' @docType data
+#' @usage LGTrappingDB_03192017
+#' @format Data from the LGTrappingDB
+#' @keywords LGTrappingDB Chinook steelhead LGR DABOM
+NULL
