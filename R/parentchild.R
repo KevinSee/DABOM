abb47936e2aabf09a81ca281206b55a7e75a5b4c
Kevin See <Kevin@qcinc.org>
2017-11-01 10:53:38 -0700

    Deleted nearly all files. They (or updated versions of them) are now in PITcleanr.

--- R/parentchild.R
+++
@@ -1,8 +0,0 @@
-#' @title Parent-child relationship of DABOM nodes.
-#'
-#'   An example of a parent-child table of model nodes listed in the site configuration file.
-#'
-#' @format A data frame with 207 rows and 5 variables
-
-#' @source Rick Orme
-"parentchild"
