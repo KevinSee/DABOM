2e25d4e06c5aab8ebd652a6d80b1ef6c3a00f452
Kevin See <Kevin@qcinc.org>
2017-12-05 14:35:47 -0800

    included capture history data as input so that number of detected tags can be included as an output.

--- R/summariseDetectProbs.R
+++ R/summariseDetectProbs.R
@@ -5,7 +5,8 @@
 #' @author Kevin See
 #'
 #' @param dabom_mod An MCMC.list, where the detection parameter names all end with "\code{_p}".
-#' @param cred_int_prob A numeric scalar in the interval (0,1) giving what higest posterior density portion of the posterior the credible interval should cover. The default value is 95%.
+#' @param capHist_proc Dataframe with the same format as that returned by \code{processCapHist_LGD}, under the name \code{ProcCapHist}. This is the data fed into DABOM as observations.
+#' @param cred_int_prob A numeric scalar in the interval (0,1) giving what higest posterior density portion of the posterior the credible interval should cover. The default value is 95\\%.
 #'
 #' @import dplyr tidyr MCMCglmm coda stringr
 #' @export
@@ -13,9 +14,13 @@
 #' @examples summariseDetectProbs()
 
 summariseDetectProbs = function(dabom_mod = NULL,
+                                capHist_proc = NULL,
                                 cred_int_prob = 0.95) {
 
-  stopifnot(!is.null(dabom_mod))
+  stopifnot(!is.null(dabom_mod) |
+              !is.null(capHist_proc))
+
+  dabom_mod = as.mcmc.list(dabom_mod)
 
   # estimate the credible interval for each parameter
   credInt = coda::HPDinterval(dabom_mod, prob = cred_int_prob) %>%
@@ -53,6 +58,12 @@ summariseDetectProbs = function(dabom_mod = NULL,
     dplyr::rename(Node = param) %>%
     dplyr::mutate(Node = stringr::str_replace(Node, '_p$', ''))
 
-  return(detect_df)
+  # add number of unique tags sighted at each node
+  detect_summ = capHist_proc %>%
+    dplyr::group_by(Node) %>%
+    dplyr::summarise(n_tags = n_distinct(TagID)) %>%
+    full_join(detect_df)
+
+  return(detect_summ)
 
 }
