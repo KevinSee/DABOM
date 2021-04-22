6567da81c59b7e458b1897e350e858525ac6c711
Kevin See <Kevin.See@merck.com>
2021-04-22 09:33:17 -0700

    updated to work with newest version of PITcleanr

--- R/compileTransProbs_PRO.R
+++ R/compileTransProbs_PRO.R
@@ -5,7 +5,7 @@
 #' @author Kevin See
 #'
 #' @param dabom_mod An MCMC.list
-#' @param parent_child dataframe created from \code{PITcleanr::createParentChildDf} or similar that contains at least ParentNode, ChildNode and river kilometer columns
+#' @inheritParams createDABOMcapHist
 #'
 #' @import dplyr tidyr stringr
 #' @export
@@ -18,19 +18,6 @@ compileTransProbs_PRO = function(dabom_mod = NULL,
   stopifnot(!is.null(dabom_mod),
             !is.null(parent_child))
 
-  # create parent-child table by site (instead of node)
-  site_parent_child = parent_child %>%
-    mutate(ParentSite = stringr::str_remove(ParentNode, 'B0$'),
-           ParentSite = stringr::str_remove(ParentSite, 'A0$'),
-           ChildSite = stringr::str_remove(ChildNode, 'B0$'),
-           ChildSite = stringr::str_remove(ChildSite, 'A0$')) %>%
-    filter(ParentSite != ChildSite) %>%
-    select(ParentSite, ChildSite, SiteType, RKM, nodeOrder) %>%
-    distinct() %>%
-    group_by(ParentSite) %>%
-    mutate(brnch_num = 1:n()) %>%
-    ungroup()
-
   # make sure dabom_mod is mcmc.list
   if(class(dabom_mod) == 'jagsUI') dabom_mod = dabom_mod$samples
 
@@ -42,43 +29,53 @@ compileTransProbs_PRO = function(dabom_mod = NULL,
                         chains = T) %>%
     as_tibble() %>%
     # pull out movement parameters
-    select(CHAIN, ITER, starts_with("p_pop_"), starts_with("phi_"))
+    select(CHAIN, ITER,
+           starts_with("p_pop_"),
+           starts_with("phi_"))
 
   trans_df = trans_mat %>%
     tidyr::gather(param, value, -CHAIN, -ITER) %>%
     mutate(origin = stringr::str_split(param, '\\[', simplify = T)[,2],
            origin = stringr::str_sub(origin, 1, 1)) %>%
-    mutate(ParentSite = stringr::str_split(param, '\\[', simplify = T)[,1],
-           ParentSite = stringr::str_remove(ParentSite, '^p_pop_'),
-           ParentSite = stringr::str_remove(ParentSite, '^phi_'),
+    mutate(parent = stringr::str_split(param, '\\[', simplify = T)[,1],
+           parent = stringr::str_remove(parent, '^p_pop_'),
+           parent = stringr::str_remove(parent, '^phi_'),
            brnch_num = stringr::str_split(param, '\\,', simplify = T)[,2],
            brnch_num = stringr::str_remove(brnch_num, '\\]')) %>%
     mutate_at(vars(brnch_num),
               list(as.numeric)) %>%
     mutate(brnch_num = if_else(is.na(brnch_num),
                                1, brnch_num)) %>%
-    left_join(site_parent_child %>%
-                group_by(ParentSite) %>%
+    left_join(parent_child %>%
+                group_by(parent) %>%
+                arrange(child_rkm) %>%
                 mutate(brnch_num = 1:n()) %>%
-                select(ParentSite, ChildSite, brnch_num)) %>%
-    mutate(ChildSite = if_else(is.na(ChildSite),
-                               paste0(ParentSite, '_bb'),
-                               ChildSite)) %>%
-    select(CHAIN, ITER, origin, ChildSite, value) %>%
-    tidyr::spread(ChildSite, value) %>%
+                select(parent, child, brnch_num),
+              by = c("parent", "brnch_num")) %>%
+    mutate(child = if_else(is.na(child),
+                           paste0(parent, '_bb'),
+                           child)) %>%
+    select(CHAIN, ITER, origin, child, value) %>%
+    tidyr::pivot_wider(names_from = "child",
+                       values_from = "value") %>%
     # multiply some probabilities together
     rowwise() %>%
-    mutate_at(vars(SM1, TP2, TOP_bb),
-              list(~ . * TOP)) %>%
-    mutate_at(vars(AH1, LNR, LWC, ROZ, SUN_bb),
-              list(~ . * SUN)) %>%
-    mutate_at(vars(LMC, TAN, SWK, LMT, ROZ_bb),
-              list(~ . * ROZ)) %>%
-    mutate_at(vars(UMC),
-              list(~ . * LMC)) %>%
+    mutate(across(c(SM1, TP2, TOP_bb),
+                  list(~ . * TOP))) %>%
+    mutate(across(c(SM1, TP2, TOP_bb),
+                  list(~ . * TOP))) %>%
+    mutate(across(c(AH1, LNR, LWC, ROZ, SUN_bb),
+                  list(~ . * SUN))) %>%
+    mutate(across(c(LMC, TAN, SWK, LMT, ROZ_bb),
+                  list(~ . * ROZ))) %>%
+    mutate(across(c(UMC),
+                  list(~ . * LMC))) %>%
     ungroup() %>%
     mutate(iter = 1:n()) %>%
-    tidyr::gather(param, value, -CHAIN, -ITER, -iter, -origin) %>%
+    tidyr::pivot_longer(cols = -c(CHAIN, ITER,
+                                  iter, origin),
+                        names_to = "param",
+                        values_to = "value") %>%
     select(chain = CHAIN,
            iter,
            origin,
