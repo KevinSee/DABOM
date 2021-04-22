0d26e8fba92d6415038023262a8917c4497c0a93
Kevin See <Kevin.See@merck.com>
2021-04-22 11:07:21 -0700

    simplified function. Returns a list named by the branching node site code, with a number describing how many child sites exist from each branching node (if more than 1).

--- R/setBranchNums.R
+++ R/setBranchNums.R
@@ -6,35 +6,20 @@
 #'
 #' @inheritParams createDABOMcapHist
 #'
-#' @import dplyr PITcleanr
-#' @importFrom rlang set_names
+#' @import dplyr tidyr
 #' @export
 #' @return NULL
 #' @examples setBranchNums()
 
 setBranchNums = function(parent_child = NULL) {
 
-  root_node = parent_child %>%
-    PITcleanr::buildNodeOrder() %>%
-    filter(node_order == 1) %>%
-    pull(node)
-
-  branchNodes = parent_child %>%
+  parent_child %>%
     dplyr::count(parent,
                  name = "n_child") %>%
     filter(n_child > 1) %>%
-    mutate(across(parent,
-                  str_replace,
-                  'A0$', '')) %>%
-    # add a "not there" to every branch except root
-    mutate(n_child = if_else(parent == root_node,
-                             n_child,
-                             as.integer(n_child + 1)))
-
-  branchNums_list = branchNodes %>%
-    pull(n_child) %>%
+    tidyr::pivot_wider(names_from = "parent",
+                       values_from = "n_child") %>%
     as.list() %>%
-    rlang::set_names(paste0('n_pops_', branchNodes$parent))
+    return()
 
-  return(branchNums_list)
 }
